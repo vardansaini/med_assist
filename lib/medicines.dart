ddecc28c30898c7d8caca123b1780c5426cac29f
Bhavnoor Kaur <bhavnoorks@gmail.com>
2021-01-16 17:04:10 -0700

    Adding profile_page and medicines_page with basic functionality

---
+++ lib/medicines.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class MedicinesPage extends StatefulWidget {
+  @override
+  _MedicinesPageState createState() => _MedicinesPageState();
+}
+
+class _MedicinesPageState extends State<MedicinesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            backgroundColor: Colors.redAccent,
+            title: Text("Medicines"),
+            centerTitle: true,
+            leading: IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () {
+                  // Navigate to MainPage
+                }
+            )
+        )
+    );
+  }
+}
